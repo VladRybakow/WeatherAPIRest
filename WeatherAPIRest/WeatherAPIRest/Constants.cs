7f9fb1c95034c6620ccc05b3007bab3ec1a8811e
Анатолий Жмышенко <gggame184@gmail.com>
2022-05-17 09:27:49 +0300

    create model

--- WeatherAPIRest/WeatherAPIRest/Constants.cs
+++ WeatherAPIRest/WeatherAPIRest/Constants.cs
@@ -6,7 +6,6 @@ namespace WeatherAPIRest
 {
     public class Constants
     {
-        public const string ApiKey = "a4a06a22d00f6e79be6c66f34c9eea07";
-        public const string RestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid={ApiKey}";
+        public const string RestUrl = "https://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&appid=a4a06a22d00f6e79be6c66f34c9eea07";
     }
 }
--- WeatherAPIRest/WeatherAPIRest/Services/TodoManager.cs
+++ WeatherAPIRest/WeatherAPIRest/Services/TodoManager.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace WeatherAPIRest.Model
+namespace WeatherAPIRest.Services
 {
-    public class EntryModel
+    public class TodoManager
     {
     }
 }
